a9d4b8698fb7ac1bb54fb74134f1751910809a4b
= <sadiinso@gmail.com>
2018-02-14 23:38:49 +0100

    [+] Optimisation + terrain infini
    [+] Le terrain est maintenant infini
    [+] réduction massive des draw calls
    [+] Création dynamique d'un texture atlas

---
+++ Assets/Scripts/WorldLoader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class WorldLoader : MonoBehaviour {
+
+    public static MonoBehaviour instance;
+
+    public GameObject player;
+    public int loadDistance;
+    private World world;
+
+	// Use this for initialization
+	void Start () {
+        instance = this;
+
+        BlockDefManager.InitBlockDefinitions();
+        BlockDefManager.BakeTextureAtlas();
+
+        Settings.loadDistance = loadDistance;
+
+        Save save = new Save("testSave");
+        world = save.GetWorld(0, World.SEED_TEST_WORLD, player.transform.position);
+
+        world.SaveLoadedChunks();
+        Debug.Log("all chunks saved");
+	}
+
+    void Update()
+    {
+        world.Update(player.transform.position);
+    }
+	
+}
