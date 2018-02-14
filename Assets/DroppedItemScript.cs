faee9823d15f7416c373c8180c5f8930918b1ed8
Théo Machon <theo.machon.91@gmail.com>
2018-02-14 17:20:41 +0100

    Ajout d'un prefab template pour objet droppé, et de leur ramassage

---
+++ Assets/DroppedItemScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroppedItemScript : MonoBehaviour {
+
+    private float vitesse = 0.1f;
+    public GameObject joueur;
+
+    void FixedUpdate()
+    {
+
+        if (Mathf.Abs(this.transform.position.x - joueur.transform.position.x) < 2 &&
+            Mathf.Abs(this.transform.position.y - joueur.transform.position.y) < 2 &&
+            Mathf.Abs(this.transform.position.z - joueur.transform.position.z) < 2)
+        {
+            this.transform.Rotate(new Vector3(0, -5, 0));
+            transform.position = Vector3.MoveTowards(transform.position, joueur.transform.position, vitesse * Time.deltaTime);
+            if (vitesse < 1.5f) vitesse += 0.05f;
+        }
+        else
+        {
+            this.transform.Rotate(new Vector3(0, 5, 0));
+            vitesse = 0.1f;
+        }
+    }
+
+}
---
+++ Assets/PickupDroppedItemScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupDroppedItemScript : MonoBehaviour {
+
+    public int nbBlankObject = 0;
+
+    void OnTriggerEnter(Collider objet)
+    {
+
+        if (objet.gameObject.CompareTag("BlankObject"))
+        {
+            objet.gameObject.SetActive(false);
+            nbBlankObject++;
+        }
+
+    }
+}
