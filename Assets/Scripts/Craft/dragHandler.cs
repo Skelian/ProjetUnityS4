360e75c5f080913ec9d7ee3927bb16ae73655048
Arthur CHAUVEAU <popteru@gmail.com>
2018-02-26 15:36:16 +0100

    Arthur 26/02 Slot et Object
    drag adn drop fonctionnel soon le craft

---
+++ Assets/Scripts/Craft/dragHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class dragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+	public static GameObject item;    // i changed itembeigdraged to item.
+
+
+	public static Transform startParent;
+	Vector3 startPosition;
+	bool start = true;
+
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		item = gameObject;
+		startPosition = transform.position;
+		startParent = transform.parent;
+
+		GetComponent<CanvasGroup>().blocksRaycasts = false;
+		item.GetComponent<LayoutElement>().ignoreLayout = true;
+		item.transform.SetParent(item.transform.parent);
+	}
+
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		transform.position = Input.mousePosition;
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		item = null;
+		transform.position = transform.parent.position;
+
+		GetComponent<CanvasGroup>().blocksRaycasts = true;
+		GetComponent<LayoutElement>().ignoreLayout = false;
+
+
+	}
+
+}
---
+++ Assets/Scripts/Craft/slot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class slot : MonoBehaviour , IDropHandler{ 
+	public GameObject item { 
+		get { 
+			if (transform.childCount > 0) {
+				return transform.GetChild(0).gameObject; 
+			} 
+			return null; 
+		} 
+	} 
+
+	#region IdropHandler implementation 
+	public void OnDrop(PointerEventData eventData) {
+		GameObject libre = item;
+		if (!libre) { 
+			dragHandler.item.transform.SetParent (transform); 
+		} else {
+			libre.transform.SetParent (dragHandler.startParent);
+			libre.transform.position= libre.transform.parent.position;
+			dragHandler.item.transform.SetParent (transform);
+		}
+	} #endregion 
+
+}﻿ 
