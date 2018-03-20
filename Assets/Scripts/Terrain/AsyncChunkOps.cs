b04dbd778dd9959462287e90bc9029c767768bb0
= <sadiinso@gmail.com>
2018-03-20 14:21:51 +0100

    correction commit précédent

---
+++ Assets/Scripts/Terrain/AsyncChunkOps.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+
+public class AsyncChunkOp
+{
+    private struct LoadingChunk
+    {
+        public Position position;
+        public World world;
+    }
+
+    private struct SavingChunk
+    {
+        public Chunk chunk;
+        public World world;
+    }
+
+    private static Thread chunkLoader;
+    private static Thread chunkSaver;
+    private static ConcurrentQueue<SavingChunk> chunksToSave = new ConcurrentQueue<SavingChunk>();
+    private static ConcurrentQueue<LoadingChunk> chunksToLoad = new ConcurrentQueue<LoadingChunk>();
+    private static ConcurrentQueue<Chunk> loadedChunks = new ConcurrentQueue<Chunk>();
+
+    public static void Init()
+    {
+        chunkLoader = new Thread(() =>
+        {
+            while(chunkLoader.IsAlive)
+            {
+                while (chunksToLoad.Count > 0)
+                {
+                    LoadingChunk toLoad;
+                    chunksToLoad.TryDequeue(out toLoad);
+
+                    Chunk loaded = Chunk.LoadChunk(toLoad.world, toLoad.position);
+                    loadedChunks.Enqueue(loaded);
+                }
+
+                Thread.Sleep(10);
+            }
+        });
+
+        chunkSaver = new Thread(() =>
+        {
+            while (chunkLoader.IsAlive)
+            {
+                while (chunksToSave.Count > 0)
+                {
+                    SavingChunk toSave;
+                    chunksToSave.TryDequeue(out toSave);
+                    Chunk.SaveChunk(toSave.world.SaveDir, toSave.chunk);
+                }
+
+                Thread.Sleep(10);
+            }
+        });
+
+        chunkLoader.Start();
+        chunkSaver.Start();
+    }
+
+    public static void AddChunkToSaveList(World world, Chunk chunkToSave)
+    {
+        // ne pas sauvegarder un chunk déja en cours de sauvegarde
+        foreach (SavingChunk saving in chunksToSave)
+            if (saving.chunk.Position == chunkToSave.Position)
+                return;
+
+        SavingChunk toSave;
+        toSave.world = world;
+        toSave.chunk = chunkToSave;
+        chunksToSave.Enqueue(toSave);
+    }
+
+    public static void AddChunkToLoadList(World world, Position chunkPos)
+    {
+        //ne pas charger un chunk déja en cours de chargement
+        foreach (LoadingChunk loading in chunksToLoad)
+            if (loading.position == chunkPos)
+                return;
+
+        LoadingChunk toLoad;
+        toLoad.world = world;
+        toLoad.position = chunkPos;
+        chunksToLoad.Enqueue(toLoad);
+    }
+
+    public static Chunk GetLoadedChunk()
+    {
+        Chunk chunk;
+        loadedChunks.TryDequeue(out chunk);
+        return chunk;
+    }
+
+    public static void ClearLoadingQueue()
+    {
+        LoadingChunk ignored;
+        while (chunksToLoad.TryDequeue(out ignored)) ;
+    }
+
+    public static void ClearLoadedQueue()
+    {
+        Chunk ignored;
+        while (loadedChunks.TryDequeue(out ignored)) ;
+    }
+
+}
+
---
+++ Assets/Scripts/Terrain/Fluid.cs
@@ -0,0 +1,52 @@
+﻿public class Fluid  {
+
+    public Block block;
+    private Block West;
+    private Block East;
+    private Block North;
+    private Block South;
+    private Block Down;
+
+
+    public Fluid(Block c)
+    {
+        block = c;
+        West = block.GetNearBlock(Utils.Face.WEST);
+        East = block.GetNearBlock(Utils.Face.EAST);
+        North = block.GetNearBlock(Utils.Face.NORTH);
+        South = block.GetNearBlock(Utils.Face.SOUTH);
+        Down = block.GetNearBlock(Utils.Face.DOWN);
+    }
+
+    // Update is called once per frame
+    public bool Update () {
+        if ((Down.ID ==Block.AIR_BLOCK_ID) || (Down.ID == 201) || (Down.ID == 202) || (Down.ID == 203))
+                Down.ParentChunk.SetLocalBlock(200, Down.Position);
+        else
+        {
+            if  ((Down.ID != 200) && (block.ID < 203))
+            {
+                
+                if (West.ID == Block.AIR_BLOCK_ID)
+                {
+                    West.ParentChunk.SetLocalBlock(block.ID + 1, West.Position);
+                }
+                if (East.ID == Block.AIR_BLOCK_ID)
+                {
+                    East.ParentChunk.SetLocalBlock(block.ID + 1, East.Position);
+                }
+                if (North.ID == Block.AIR_BLOCK_ID)
+                {
+                    North.ParentChunk.SetLocalBlock(block.ID + 1, North.Position);
+                }
+                if (South.ID == Block.AIR_BLOCK_ID)
+                {
+                    South.ParentChunk.SetLocalBlock(block.ID + 1, South.Position);
+                }
+                
+            }
+        }
+
+        return false;
+    }
+}
