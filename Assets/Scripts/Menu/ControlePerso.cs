ee0ba3485f9dcde2b182a1c21b45620296c4439a
Guillaume Boutry <boutryguillaume1@gmail.com>
2018-03-20 15:22:42 +0100

    Sauvegarde des contrôles entre les sessions

--- Assets/Scripts/Menu/ControlePerso.cs
+++ Assets/Scripts/Menu/ControlePerso.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
 
 public class ControlePerso : MonoBehaviour {
 	public GameObject btnAttaquer, btnAvancer,btnArriere,btnGauche,btnUtiliser,btnSauter,btnDroite,btnRun;
+
 
 	public static KeyCode ATTAQUER = KeyCode.Mouse0,
 		UTILISER = KeyCode.Mouse1,
@@ -18,6 +22,12 @@ public class ControlePerso : MonoBehaviour {
 
 	bool chg = false;
 	KeyCode toChg;
+
+
+	void Start(){
+		Load ();
+	}
+
 	void Update(){
 		if (chg) {
 			foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode))) {
@@ -100,5 +110,68 @@ public class ControlePerso : MonoBehaviour {
 	public void changerCourir(){
 		chg = true;
 		toChg = COURIR;
+	}
+
+	public void Save(){
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create (Application.persistentDataPath + "/controles.gd");
+		List<KeyCode> keycodes = new List<KeyCode>();
+
+		keycodes.Add (ATTAQUER);
+		keycodes.Add (UTILISER);
+		keycodes.Add (AVANCER);
+		keycodes.Add (GAUCHE);
+		keycodes.Add (DROITE);
+		keycodes.Add (ARRIERE);
+		keycodes.Add (COURIR);
+		keycodes.Add (SAUTER);
+
+		bf.Serialize(file, keycodes);
+		file.Close();
+	}
+
+	public void DefaultControls(){
+		ATTAQUER = KeyCode.Mouse0;
+		UTILISER = KeyCode.Mouse1;
+		AVANCER = KeyCode.Z;
+		ARRIERE = KeyCode.S;
+		GAUCHE = KeyCode.Q;
+		DROITE = KeyCode.D;
+		SAUTER = KeyCode.Space;
+		COURIR = KeyCode.LeftShift;
+		UpdateKeycodes ();
+	}
+
+	public void Load(){
+		if(File.Exists(Application.persistentDataPath + "/controles.gd")) {
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/controles.gd", FileMode.Open);
+
+			List<KeyCode> keycodes = (List<KeyCode>)bf.Deserialize(file);
+			ATTAQUER = keycodes [0];
+			UTILISER = keycodes [1];
+			AVANCER = keycodes [2];
+			GAUCHE = keycodes [3];
+			DROITE = keycodes [4];
+			ARRIERE = keycodes [5];
+			COURIR = keycodes [6];
+			SAUTER = keycodes [7];
+
+			UpdateKeycodes ();
+
+			file.Close();
+		}
+	}
+
+	public void UpdateKeycodes(){
+		btnAttaquer.transform.GetChild(0).GetComponent<Text>().text = ATTAQUER.ToString(); 
+		btnUtiliser.transform.GetChild(0).GetComponent<Text>().text = UTILISER.ToString(); 
+		btnGauche.transform.GetChild(0).GetComponent<Text>().text = GAUCHE.ToString(); 
+		btnDroite.transform.GetChild(0).GetComponent<Text>().text = DROITE.ToString(); 
+		btnAvancer.transform.GetChild(0).GetComponent<Text>().text = AVANCER.ToString(); 
+		btnArriere.transform.GetChild(0).GetComponent<Text>().text = ARRIERE.ToString(); 
+		btnRun.transform.GetChild(0).GetComponent<Text>().text = COURIR.ToString(); 
+		btnSauter.transform.GetChild(0).GetComponent<Text>().text = SAUTER.ToString(); 
+
 	}
 }
