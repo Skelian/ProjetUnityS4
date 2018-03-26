6032df2e593d03423d57d835dfff2118497f8303
Lirewyre <lirewyre@users.noreply.github.com>
2018-03-26 11:48:15 +0200

    Raycast fixed

--- Assets/Scripts/Player/GrabRaycast.cs
+++ Assets/Scripts/Player/GrabRaycast.cs
@@ -5,7 +5,6 @@ using UnityEngine.EventSystems;
 
 public class GrabRaycast : MonoBehaviour {
 
-    public object objectIntersected;
 
     private RaycastHit vision;
     private PhysicsRaycaster pr;
@@ -15,23 +14,14 @@ public class GrabRaycast : MonoBehaviour {
         pr = GetComponent<PhysicsRaycaster>();
     }
 
-    void Update() {
-
-        if (Physics.Raycast(GetComponent<GameObject>().transform.parent.position, Vector3.forward, out vision, 5.0f))
+	void FixedUpdate() {
+		if ((Physics.Raycast(GetComponent<Transform>().position, Vector3.forward, out vision, 8.0f)) && (Input.GetKeyDown(ControlePerso.ATTAQUER))
         {
             // Joueur à portée de combat ou bloc à portée de minage : on garde l'objet en mémoire
-            if (((vision.collider.tag == "Player") && (vision.distance <= 2)) || ((vision.collider.tag == "Block") && (vision.distance <= 3)))
-            {
-                objectIntersected = vision;
-                Debug.Log("object in vision: " + vision.collider.tag);
-            } else {
-                objectIntersected = null;
-            }
-
-        } else {
-
-            objectIntersected = null;
-
+			if (((vision.collider.tag == "perso") && (vision.distance <= 3)) || ((vision.collider.name.StartsWith("chunk")) && (vision.distance <= 5)))
+			{
+				vision.collider.gameObject.hit();
+			}
         }
     }
 }
