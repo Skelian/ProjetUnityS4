adaa5d073eee29a91a37a3d64aa59ae7e55b8c51
Lirewyre <d.mazinsaidi@gmail.com>
2018-03-27 15:01:42 +0200

    Raycast action commented, waiting for hit() function for players and blocks Adding files of Allan and Alexis Ly so as to implementing (a problem on my computer stops me from integrating it)

---
+++ Assets/Scripts/Player/ChangementCam.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ChangementCam : MonoBehaviour {
+
+	public GameObject head;
+	public GameObject camTPS;
+	bool cameraChange=false;
+
+	void Update () {
+
+
+		if (Input.GetKeyDown (KeyCode.M)) {
+			if (!cameraChange) {
+				transform.position = camTPS.transform.position;
+				cameraChange = true;
+			} else {
+				transform.position = head.transform.position;
+				cameraChange = false;
+			}
+		}
+	}
+}
+
+/* Mettre un objet a l'endroit ou l'on veut mettre la camera 3eme personne.
+ * Decocher le box collider.
+ * Mettre l'objet dans la variable cameraTPS
+ * Mettre la tete du personnage dans la variable head.
+ * Mettre le cube en enfant de la tete
+ * Mettre le script dans la camera.
+ * Mettre le cube en invisible
+ */
--- Assets/Scripts/Player/GrabRaycast.cs
+++ Assets/Scripts/Player/GrabRaycast.cs
@@ -15,12 +15,14 @@ public class GrabRaycast : MonoBehaviour {
     }
 
 	void FixedUpdate() {
-		if ((Physics.Raycast(GetComponent<Transform>().position, Vector3.forward, out vision, 8.0f)) && (Input.GetKeyDown(ControlePerso.ATTAQUER))
+		if ((Physics.Raycast(GetComponent<Transform>().position, Vector3.forward, out vision, 8.0f)) && (Input.GetKeyDown(ControlePerso.ATTAQUER)))
         {
             // Joueur à portée de combat ou bloc à portée de minage : on garde l'objet en mémoire
 			if (((vision.collider.tag == "perso") && (vision.distance <= 3)) || ((vision.collider.name.StartsWith("chunk")) && (vision.distance <= 5)))
 			{
-				vision.collider.gameObject.hit();
+				/*
+                    vision.collider.gameObject.hit();
+                */
 			}
         }
     }
