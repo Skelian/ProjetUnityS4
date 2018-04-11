91c418cab6fdb03af0c90eaf40da6d76d1662615
nakatchi <cezard.florian@gmail.com>
2018-04-11 15:28:14 +0200

    SpaceCraft YAY

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -2,27 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
 using System;
 
 public class Menu : MonoBehaviour {
 	public GameObject mainM;
 	public GameObject createW;
 	public GameObject loadW;
+	public GameObject multi;
 	public GameObject options;
 	public GameObject GameCtrl;
+	public GameObject rejServ;
+	public GameObject créerServ;
 	public InputField seedTxt;
+	public InputField seedTxt2;
+	public InputField port;
 
 	public void GoToMainMenu(){
 		createW.SetActive (false);
 		loadW.SetActive (false);
 		options.SetActive (false);
+		multi.SetActive (false);
 		mainM.SetActive (true);
 	}
-
+	public void MultiplayerMenu(){
+		multi.SetActive (true);
+		mainM.SetActive (false);
+	}
+	public void rejoindreServeur(){
+		multi.SetActive (false);
+		rejServ.SetActive (true);
+	}
+	public void créerServeur(){
+		multi.SetActive (false);
+		créerServ.SetActive (true);
+		int seed = generateSeed ();
+		port.text = "7777";
+	}
+	public void annulerMulti(){
+		rejServ.SetActive (false);
+		créerServ.SetActive (false);
+		multi.SetActive (true);
+	}
 	public void CreateWorld(){
 		createW.SetActive (true);
 		mainM.SetActive (false);
 		int seed = generateSeed ();
+
 	}	
 	public void LoadWorld(){
 		loadW.SetActive (true);
@@ -58,6 +84,8 @@ public class Menu : MonoBehaviour {
 
 		print ("SEED: " + seed + " HASHCODE : " + seed.GetHashCode ().ToString ());
 		seedTxt.text = seed;
+		seedTxt2.text = seed;
+
 		return seed.GetHashCode ();
 	}
 }
