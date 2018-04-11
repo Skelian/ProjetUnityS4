3fe2065411ead92e3b5a5adf1bb55701051fab90
Guillaume Boutry <boutryguillaume1@gmail.com>
2018-04-11 15:53:02 +0200

    Intégration multijoueur avec menu

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor;
 using System;
 
 public class Menu : MonoBehaviour {
---
+++ Assets/Scripts/Multiplayer/NetworkManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviour {
+	
+	NetworkClient myClient;
+	private UnityEngine.Networking.NetworkManager nm;
+
+    public InputField portTxtHost, portTxtJoin, hostIp;
+
+    public enum Port
+    {
+        HOST,
+        JOIN
+    };
+
+	// Use this for initialization
+	void Start () {
+		nm = GetComponent<UnityEngine.Networking.NetworkManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetupServer(){
+		/*int port = UpdatePort ();
+		NetworkServer.Listen (port);*/
+		nm.StartServer ();
+
+	}
+
+	public void SetupClient(){
+		/*int port = UpdatePort ();
+		string ip = UpdateServerIp ();
+		myClient = new NetworkClient ();
+		myClient.RegisterHandler (MsgType.Connect, OnConnected);
+		myClient.Connect ("127.0.0.1", 4444);*/
+		nm.StartClient ();
+	}
+
+	public void SetupLocalClient(){
+		/*myClient = ClientScene.ConnectLocalServer ();
+		myClient.RegisterHandler (MsgType.Connect, OnConnected);*/
+		nm.StartClient ();
+	}
+
+	public void Host(){
+        nm.matchPort = UpdatePort(Port.HOST);
+		nm.StartHost ();
+        nm.StartClient();
+	}
+
+    public void Join()
+    {
+        nm.matchPort = UpdatePort(Port.JOIN);
+        nm.serverBindAddress = UpdateServerIp();
+        nm.StartClient();
+    }
+
+    public int UpdatePort (Port code){
+        string port;
+        switch (code)
+        {
+            case Port.HOST:
+                port = portTxtHost.text;
+                break;
+            case Port.JOIN:
+                port = portTxtJoin.text;
+                break;
+            default:
+                port = "7777";
+                break;
+        }
+
+        return int.Parse(port);
+    }
+
+	public string UpdateServerIp(){
+		return hostIp.text;
+	}
+
+	public void OnConnected(NetworkMessage netMsg){
+		Debug.Log ("Connected");
+	}
+}
